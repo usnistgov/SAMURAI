641fbd9be22920d2102a45bddfd1d9d46ace0b34
aweiss <alec.weiss@nist.gov>
2019-06-25 15:07:14 -0600

    started adding stuff

--- analysis/support/MUFResult.py
+++ analysis/support/MUFResult.py
@@ -6,12 +6,24 @@ Created on Mon Jun 24 16:02:04 2019
 """
 
 from samurai.analysis.support.snpEditor import SnpEditor,WnpEditor
+import os
+import re
 
-class MUFResult(WnpEditor):
+class MUFResult(SnpEditor):
     '''
     @brief a class to deal with MUF results (and easily get uncertainties, monte_carlos, and whatnot)
     '''
-    def __init__(self,meas_path):
+    def __init__(self,meas_path,**arg_options):
         '''
         @brief load up and initialize the *.meas file
         @param[in] meas_path - path to the *.meas file to load
+        @param[in/OPT] arg_options - keyword arguments as follows:
+            None yet!
+            all arguments also passed to SnpEditor constructor
+        '''
+        
+        #lets set the correct options for w/s params
+        _,ext = os.path.splitext(meas_path):
+            #use re to get wp or sp (remove all numbers and '_binary')
+        ext_no_bin = ext.replace('_binary','')
+        if ext_bin
