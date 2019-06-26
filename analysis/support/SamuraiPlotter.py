d9c5ae812300f24a34f29f0638647f7ed7702b38
aweiss <alec.weiss@nist.gov>
2019-06-26 17:05:45 -0600

    added 2D matplotlib

--- analysis/support/SamuraiPlotter.py
+++ analysis/support/SamuraiPlotter.py
@@ -32,7 +32,7 @@ class SamuraiPlotter:
         self.plotly = None #plotly 
         self.matplotlib = None #matplotlib
         
-    def run_plot_function(self,plot_funct_name,*args,**kwargs):
+    def _run_plot_function(self,plot_funct_name,*args,**kwargs):
         '''
         @brief loop through each of our programs to try and plot
         @param[in] plot_funct_name - name of plotting function (ie surf)
@@ -43,11 +43,13 @@ class SamuraiPlotter:
         for pp in self.options['plot_order']:
             funct_name = '_'+plot_funct_name+'_'+pp
             funct = getattr(self,funct_name)
-            try: rv = funct(*args,**kwargs)
-            except: print("WARNING: {} plotter not found".format(pp)); continue #if it doesnt work, try the next one
-            else:   break #otherwise we are good
+            try: 
+                rv = funct(*args,**kwargs)
+            except Exception as e: 
+                print(e)
+                print("{} Failed, trying next engine".format(pp))
         if not rv: #if nothing is returned
-            raise Exception("No plotting library found")
+            raise Exception("No plotting library found or no value returned")
         return rv
         
     
@@ -68,7 +70,7 @@ class SamuraiPlotter:
                 colorbar - arguements (as a tuple) to pass to the colorbar function
         '''
         kwargs = {k.lower():v for k,v in kwargs.items()} #make all keys lowercase
-        rv = self.run_plot_function('surf',*args,**kwargs)
+        rv = self._run_plot_function('surf',*args,**kwargs)
         return rv
         
     def _surf_matlab(self,*args,**kwargs): 
@@ -166,22 +168,35 @@ class SamuraiPlotter:
     ###########################################################################
     ####### regular 2D plots
     ###########################################################################
-    #def plot(self,*args,**kwargs):
-    #    '''
-    #    @brief abstracted 2D plotting function
-    #    @param[in] args - arguments for the plot function (in MATLAB fasion)
-    #    @param[in] kwargs - keyword arguments will be used as name/value pairs
-    #        again in MATLAB format except for the following special keywords:
-    #            xlim,ylim,zlim - set the x,y,z limits on the plot
-    #            xlabel,ylable,zlabel - set the x,y,z labels on the plot
-    #            view - set the plot view
-    #            shading - set the shading (for matlab. usually 'interp')
-    #            title - title of the plot
-    #            colorbar - arguements (as a tuple) to pass to the colorbar function
-    #    '''
-    #    kwargs = {k.lower():v for k,v in kwargs.items()} #make all keys lowercase
-    #    rv = self.run_plot_function('plot',*args,**kwargs)
-    #    return rv
+    def plot(self,*args,**kwargs):
+        '''
+        @brief abstracted 2D plotting function
+        @param[in] args - arguments for the plot function (in MATLAB fasion)
+        @param[in] kwargs - keyword arguments will be used as name/value pairs
+            again in MATLAB format except for the following special keywords:
+                xlim,ylim,zlim - set the x,y,z limits on the plot
+                xlabel,ylable,zlabel - set the x,y,z labels on the plot
+                view - set the plot view
+                shading - set the shading (for matlab. usually 'interp')
+                title - title of the plot
+                colorbar - arguements (as a tuple) to pass to the colorbar function
+        '''
+        kwargs = {k.lower():v for k,v in kwargs.items()} #make all keys lowercase
+        rv = self._run_plot_function('plot',*args,**kwargs)
+        return rv
+    
+    def _plot_matplotlib(self,*args,**kwargs):
+        '''
+        @brief 1D plot in matplotlib
+        '''
+        self._check_arg_count(len(args),2,2) #must have exactly 2 args
+        self._init_matplotlib()
+        fig = self.matplotlib.figure()
+        ax = fig.gca()
+        kwargs = self._matlab2matplotlib(ax,**kwargs)
+        ax.plot(*args,**kwargs)
+        return fig
+
     
     ###########################################################################
     ####### for all unimplemented, try to call from the first value in plot order
@@ -232,13 +247,78 @@ class SamuraiPlotter:
             from mpl_toolkits.mplot3d import Axes3D
             self.plotly = ploff
             self.plotly_gobj = go
+
+    ###########################################################################
+    ####### generic internal funcitons
+    ########################################################################### 
+    def _check_arg_count(self,arg_count,min_args,max_args):
+        '''
+        @brief check for a minimum and maximum *args count
+        @param[in] arg_count - number of arguemnts recieved
+        @param[in] min_args - minimum number of arguements required
+        @param[in] max_args - maximum number of arguments possible
+        '''
+        if arg_count<min_args: 
+            raise TypeError("At least 3 input arguments required")
+        if arg_count>max_args: 
+            raise TypeError("Too many arguments (3 or 4 expected, {} recieved)".format(arg_count))
+        
+    
+    ###########################################################################
+    ####### argument translation
+    ########################################################################### 
+    def _matlab2matplotlib(self,ax,**kwargs):
+        '''
+        @brief this will run the corresponding functions for provided matlab key/val arguemnts
+            (e.g. if xlim is passed, run ax.set_xlim). 
+            This will only remove key value pairs with specified functions
+        @param[in] ax - axis to run the arguments on
+        @param[in/OPT] kwargs - keyword args to run
+        @return kwargs dictionary with specified functions removed
+        '''
+        ax_funct_dict = {}
+        #add limits to dictionary
+        for d in ['x','y','z']:
+            ax_funct_dict.update({'{}lim'.format(d):'set_{}lim'.format(d)})
+        #add labels
+        for d in ['x','y','z']:
+            ax_funct_dict.update({'{}label'.format(d):'set_{}label'.format(d)})
             
-            
+        #now get a list of the functions specified and remove from kwargs
+        funct_list = list(ax_funct_dict.values())
+        arg_dict = {k:kwargs.pop(k,None) for k in funct_list} #get our function dictionary
+        arg_dict = {k:v for k,v in arg_dict.items() if v is not None} #remove none values (not provided)
+        
+        #now loop through and run the functions with the arguments
+        #ax_funct_return_dict = {} #return 'function':rv key/value pair
+        for k,v in arg_dict.items():
+            funct = getattr(ax,ax_funct_dict[k]) #now get the method to run form the axis
+            rv = funct(v)
+            #ax_funct_return_dict[v,rv] #matplotlib function name/return-value
+        return kwargs
+    
+    
             
 if __name__=='__main__':
-    sp = SamuraiPlotter(verbose=True,plot_order=['plotly','matlab'])
-    [X,Y] = np.mgrid[1:10:0.5,1:20]
-    Z = np.sin(X)+np.cos(Y)
-    #sp.plotly(X,Y,Z,xlim=[0,20],zlabel='X',shading='interp')
-    #sp._surf_plotly(X,Y,Z,xlim=[0,20],zlabel='X',shading='interp',colorbar=('XTick',[-1,1],'XTickLabel',['A','B']))
-    sp._surf_matlab(X,Y,Z,xlim=[0.,20.],zlabel='X',shading='interp',colorbar=('XTick',[-1.,1.],'XTickLabel',['A','B']))
+    plot_test = True
+    surf_test = False
+    if surf_test:
+        sp = SamuraiPlotter(verbose=True,plot_order=['plotly','matlab'])
+        [X,Y] = np.mgrid[1:10:0.5,1:20]
+        Z = np.sin(X)+np.cos(Y)
+        #sp.plotly(X,Y,Z,xlim=[0,20],zlabel='X',shading='interp')
+        #sp._surf_plotly(X,Y,Z,xlim=[0,20],zlabel='X',shading='interp',colorbar=('XTick',[-1,1],'XTickLabel',['A','B']))
+        sp._surf_matlab(X,Y,Z,xlim=[0.,20.],zlabel='X',shading='interp',colorbar=('XTick',[-1.,1.],'XTickLabel',['A','B']))
+    if plot_test:
+        sp = SamuraiPlotter(verbose=True,plot_order=['matplotlib'])
+        x = np.linspace(0,2*np.pi,1000)
+        y = np.sin(x*5)
+        sp.plot(x,y)
+    
+    
+    
+    
+    
+    
+    
+    
