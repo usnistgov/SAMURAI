e4c864356e0c16a665c9c0ddd0c3e7e9e0018e95
aweiss <alec.weiss@nist.gov>
2019-11-20 09:14:10 -0700

    fixed some bugs and added unittesting script to better verify code before merging

---
+++ run_unittests.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 16:14:52 2019
+run different unittests
+@author: ajw5
+"""
+import unittest
+#%% initialize some things
+test_list = [] #classes of unittests to run
+
+#%% Touchstone Editor unit testing
+from samurai.base.TouchstoneEditor import TestTouchstoneEditor
+test_list.append(TestTouchstoneEditor)
+
+#%% MUF Result testing
+from samurai.analysis.support.MUFResult import TestMUFResult
+test_list.append(TestMUFResult)
+
+#%% now run them all
+import time
+time.sleep(0.5) #sleep for a bit to let loaded modules be printed
+loader = unittest.TestLoader()
+suite_list = [loader.loadTestsFromTestCase(tc) for tc in test_list]
+unittest.TextTestRunner(verbosity=2).run(unittest.TestSuite(suite_list))
