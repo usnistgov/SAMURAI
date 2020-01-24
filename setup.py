227deb8227b2f4e421edea19dfa7e9f77a213512
aweiss <alec.weiss@nist.gov>
2020-01-24 10:08:33 -0700

    fixed issues with bad install previously

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from setuptools import setup,find_packages
 
-setup(name='Distutils',
+setup(name='samurai',
       version='0.1',
       description='Synthetic aperture measurements with uncertainty and angle of incidence (SAMURAI) code.',
       author='Alec Weiss, Ben Jamroz, Rob Jones, NIST',
       author_email='alec.weiss@nist.gov',
-      packages=find_packages(where='samurai'),
+      packages=find_packages(),
       python_requires='>3.3,<4',
-      package_dir={'': 'samurai'},
      )
-
 
 #use `pip install -e .` for editable install
 
