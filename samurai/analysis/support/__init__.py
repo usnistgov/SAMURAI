0c99444871ca1a6e6c5ca5f123d753b1a78085f4
Alec Weiss <ajw5@NIST.GOV>
2020-03-27 13:07:57 -0600

    moved MUF support into base (no longer analysis.support)

--- samurai/analysis/support/__init__.py
+++ samurai/analysis/support/__init__.py
@@ -2,4 +2,5 @@ import sys
 import importlib
 
 sys.modules['samurai.analysis.support.MetaFileController'] = importlib.import_module('samurai.analysis.support.MetafileController')
-
+sys.modules['samurai.analysis.support.MUFResult'] = importlib.import_module('samurai.base.MUF.MUFResult')
+sys.modules['samurai.analysis.support.MUF'] = importlib.import_module('samurai.base.MUF')
